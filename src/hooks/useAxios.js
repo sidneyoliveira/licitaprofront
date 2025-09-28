791c5b41e3116d2d6cf89c7557b8a24d085da237
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-09-28 12:34:02 -0300

    page correcao hookes

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -4,7 +4,7 @@ import { jwtDecode } from 'jwt-decode';
 import AuthContext from '../context/AuthContext';
 
 // Importa a instância base para obter a URL, garantindo consistência
-import axiosInstance from '../api/axiosInstance';
+import axiosInstance from '../api/AxiosInstance';
 const baseURL = axiosInstance.defaults.baseURL;
 
 const useAxios = () => {
