3aba7d60766da028916ba1affdfae17837a76ec2
sidneyoliveira <sidneyoliveira2015@gmail.com>
2026-04-02 00:11:42 -0300

    melhora

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 
-const API_URL = process.env.REACT_APP_API_URL || "https://l3solution.net.br/api/";
+const API_URL = process.env.REACT_APP_API_URL || "http://l3solution.net.br/api/";
 
 export const API_BASE_URL = API_URL;
